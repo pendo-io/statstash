6a7b68926e5399f8f89db5e51c4e8775126e772e
Scott Parkerson <sgp@pendo.io>
2014-10-03 14:23:24 -0400

    Periodic flushing handler to wire in as cron job

---
+++ periodic.go
@@ -0,0 +1,32 @@
+// Copyright 2014 pendo.io
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package statstash
+
+import (
+	"appengine"
+	"net/http"
+	"time"
+)
+
+func PeriodicStatsFlusher(flusher StatsFlusher, cfg *FlusherConfig, w http.ResponseWriter, r *http.Request) {
+	c := appengine.NewContext(r)
+	sii := StatInterfaceImplementation{c}
+
+	startOfLastPeriod := getFlushPeriodStart(time.Now(), -1)
+	if err := sii.UpdateBackend(startOfLastPeriod, flusher, cfg, false); err != nil {
+		c.Errorf("Failed updating stats backend: %s", err)
+	} else {
+		c.Infof("Updated stats backend")
+	}
+}
